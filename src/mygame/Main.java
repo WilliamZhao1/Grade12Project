4c7e8f8f15688d7610796e795179f54678dbf706
Leo1478 <leozeng1478@gmail.com>
2020-12-20 15:30:47 -0500

    tree and light test
    added tree model and lighting for test

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,10 +1,13 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -29,6 +32,12 @@ public class Main extends SimpleApplication {
         geom.setMaterial(mat);
 
         rootNode.attachChild(geom);
+        
+        
+        initTreeTest();
+        initLight();
+        
+        
     }
 
     @Override
@@ -39,5 +48,20 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+    
+    
+    void initTreeTest(){
+        
+        Spatial model = assetManager.loadModel("Models/first drawing tree.glb");
+        rootNode.attachChild(model);
+        
+    }
+    
+    public void initLight(){
+        DirectionalLight dl = new DirectionalLight();
+        dl.setColor(ColorRGBA.White);
+        dl.setDirection(new Vector3f(12f, -12f, 12f).normalizeLocal());
+        rootNode.addLight(dl);
     }
 }
